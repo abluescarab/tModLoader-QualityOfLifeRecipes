0a6eaa3568396ae7fadd27e7bbe1603c98cd8b19
Alana Gilston <abluescarab@users.noreply.github.com>
2017-07-22 01:25:39 -0700

    Added slime banners

---
+++ Items/Placeable/Banners/Slimes/ElementalSlimeBanner.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace QualityOfLifeRecipes.Items.Placeable.Banners.Slimes {
+    public class ElementalSlimeBanner : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Elemental Slime Banner");
+            Tooltip.SetDefault(
+                "Nearby players get a bonus against: Sand/Ice/Jungle/Lava/Spiked Slimes");
+        }
+
+        public override void SetDefaults() {
+            item.value = Item.sellPrice(0, 0, 15, 0);
+            item.rare = 2;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = mod.TileType("ElementalSlimeBanner");
+            item.placeStyle = 0;
+        }
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SandSlimeBanner);
+            recipe.AddIngredient(ItemID.IceSlimeBanner);
+            recipe.AddIngredient(ItemID.SpikedIceSlimeBanner);
+            recipe.AddIngredient(ItemID.JungleSlimeBanner);
+            recipe.AddIngredient(ItemID.SpikedJungleSlimeBanner);
+            recipe.AddIngredient(ItemID.LavaSlimeBanner);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/Banners/Slimes/SupremeSlimeBanner.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace QualityOfLifeRecipes.Tiles.Banners.Slimes {
+    public class SupremeSlimeBanner : ModTile {
+        public override void SetDefaults() {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2Top);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 };
+            TileObjectData.newTile.StyleHorizontal = true;
+
+            ModTranslation translation = CreateMapEntryName();
+            translation.SetDefault("Supreme Slime Banner");
+
+            AddMapEntry(new Color(200, 200, 200), translation);
+
+            disableSmartCursor = true;
+
+            TileObjectData.addTile(Type);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+            Item.NewItem(i * 16, j * 16, 16, 48, mod.ItemType("SupremeSlimeBanner"));
+        }
+
+        public override void NearbyEffects(int i, int j, bool closer) {
+            if(closer) {
+                Player player = Main.LocalPlayer;
+
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.GreenSlime)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.BlueSlime)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.PurpleSlime)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.RedSlime)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.YellowSlime)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.BlackSlime)] = true;
+
+                player.hasBanner = true;
+            }
+        }
+    }
+}
