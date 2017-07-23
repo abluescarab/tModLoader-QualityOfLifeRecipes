b5e1b2300e6aeb7ed7d7cf259986aa00659394bb
Alana Gilston <alana@abluescarab.us>
2017-07-23 01:39:13 -0700

    Added dungeon banners

---
+++ Items/Placeable/Banners/Dungeon/DungeonBanner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace QualityOfLifeRecipes.Items.Placeable.Banners.Dungeon {
+    public class DungeonBanner : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Dungeon Banner");
+            Tooltip.SetDefault(
+                "Nearby players get a bonus against: Angry Bones/Dark Caster/Cursed Skull");
+        }
+
+        public override void SetDefaults() {
+            item.value = Item.sellPrice(0, 0, 10, 0);
+            item.rare = 2;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = mod.TileType("DungeonBanner");
+            item.placeStyle = 0;
+        }
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.AngryBonesBanner);
+            recipe.AddIngredient(ItemID.SkeletonMageBanner);
+            recipe.AddIngredient(ItemID.CursedSkullBanner);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Items/Placeable/Banners/Dungeon/UltimateDungeonBanner.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace QualityOfLifeRecipes.Items.Placeable.Banners.Dungeon {
+    public class UltimateDungeonBanner : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Ultimate Dungeon Banner");
+            Tooltip.SetDefault(
+                "Nearby players get a bonus against: Dungeon Enemies");
+        }
+
+        public override void SetDefaults() {
+            item.value = Item.sellPrice(0, 0, 35, 0);
+            item.rare = 2;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = mod.TileType("UltimateDungeonBanner");
+            item.placeStyle = 0;
+        }
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod, "SupremeDungeonBanner");
+            recipe.AddIngredient(ItemID.PaladinBanner);
+            recipe.AddIngredient(ItemID.SkeletonCommandoBanner);
+            recipe.AddIngredient(ItemID.SkeletonSniperBanner);
+            recipe.AddIngredient(ItemID.TacticalSkeletonBanner);
+            recipe.AddIngredient(ItemID.BoneLeeBanner);
+            recipe.AddIngredient(ItemID.DungeonSpiritBanner);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
