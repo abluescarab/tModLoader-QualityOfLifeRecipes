3621930b5dadb573f64a43a1d799ce7c889e8b4c
Alana Gilston <abluescarab@users.noreply.github.com>
2017-07-23 03:31:36 -0700

    Added Gobin Army Banner

---
+++ Items/Placeable/Banners/Events/GoblinArmyBanner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace QualityOfLifeRecipes.Items.Placeable.Banners.Events {
+    public class GoblinArmyBanner : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Goblin Army Banner");
+            Tooltip.SetDefault(
+                "Nearby players get a bonus against: Goblin Archers/Peons/Sorcerers/Thieves/Warriors");
+        }
+
+        public override void SetDefaults() {
+            item.value = Item.sellPrice(0, 0, 12, 0);
+            item.rare = 2;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = mod.TileType("GoblinArmyBanner");
+            item.placeStyle = 0;
+        }
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.GoblinArcherBanner);
+            recipe.AddIngredient(ItemID.GoblinPeonBanner);
+            recipe.AddIngredient(ItemID.GoblinSorcererBanner);
+            recipe.AddIngredient(ItemID.GoblinThiefBanner);
+            recipe.AddIngredient(ItemID.GoblinWarriorBanner);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/Banners/Events/GoblinArmyBanner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace QualityOfLifeRecipes.Tiles.Banners.Events {
+    public class GoblinArmyBanner : ModTile {
+        public override void SetDefaults() {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2Top);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 12 };
+            TileObjectData.newTile.StyleHorizontal = true;
+
+            ModTranslation translation = CreateMapEntryName();
+            translation.SetDefault("Goblin Army Banner");
+
+            AddMapEntry(new Color(200, 200, 200), translation);
+
+            disableSmartCursor = true;
+
+            TileObjectData.addTile(Type);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+            Item.NewItem(i * 16, j * 16, 16, 48, mod.ItemType("GoblinArmyBanner"));
+        }
+
+        public override void NearbyEffects(int i, int j, bool closer) {
+            if(closer) {
+                Player player = Main.LocalPlayer;
+
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.GoblinArcher)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.GoblinPeon)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.GoblinSorcerer)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.GoblinThief)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.GoblinWarrior)] = true;
+                
+                player.hasBanner = true;
+            }
+        }
+    }
+}
