cdce8e2d7bce557ebede48320edef3607db4d739
Alana Gilston <alana@abluescarab.us>
2017-07-23 05:03:15 -0700

    Added Undead Banner

---
+++ Items/Placeable/Banners/UndeadBanner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace QualityOfLifeRecipes.Items.Placeable.Banners {
+    public class UndeadBanner : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Undead Banner");
+            Tooltip.SetDefault(
+                "Nearby players get a bonus against: Zombie, Raincoat Zombie, Demon Eye");
+        }
+
+        public override void SetDefaults() {
+            item.value = Item.sellPrice(0, 0, 5, 0);
+            item.rare = 2;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = mod.TileType("UndeadBanner");
+            item.placeStyle = 0;
+        }
+
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.ZombieBanner);
+            recipe.AddIngredient(ItemID.DemonEyeBanner);
+            recipe.AddIngredient(ItemID.RaincoatZombieBanner);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/Banners/UndeadBanner.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace QualityOfLifeRecipes.Tiles.Banners.Events {
+    public class UndeadBanner : ModTile {
+        public override void SetDefaults() {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2Top);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 12 };
+            TileObjectData.newTile.StyleHorizontal = true;
+
+            ModTranslation translation = CreateMapEntryName();
+            translation.SetDefault("Undead Banner");
+
+            AddMapEntry(new Color(200, 200, 200), translation);
+
+            disableSmartCursor = true;
+
+            TileObjectData.addTile(Type);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+            Item.NewItem(i * 16, j * 16, 16, 48, mod.ItemType("UndeadBanner"));
+        }
+
+        public override void NearbyEffects(int i, int j, bool closer) {
+            if(closer) {
+                Player player = Main.LocalPlayer;
+
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.Zombie)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.ArmedZombie)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.BaldZombie)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.PincushionZombie)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.ArmedZombiePincussion)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.SlimedZombie)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.ArmedZombieSlimed)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.SwampZombie)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.ArmedZombieSwamp)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.TwiggyZombie)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.ArmedZombieTwiggy)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.FemaleZombie)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.ArmedZombieCenx)] = true;
+                player.NPCBannerBuff[Item.NPCtoBanner(NPCID.ZombieRaincoat)] = true;
+
+                player.hasBanner = true;
+            }
+        }
+    }
+}
