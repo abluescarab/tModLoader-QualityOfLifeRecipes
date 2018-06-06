53a2679a221b54a26425f7c69787580a039a7b26
Alana Gilston <alana@abluescarab.us>
2018-06-06 05:54:30 -0700

    Added Angler's Fishing Gear, Atomic Cloud Boots

---
+++ Items/Accessories/AnglersFishingGear.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace QualityOfLifeRecipes.Items.Accessories {
+    [AutoloadEquip(EquipType.Back)]
+    public class AnglersFishingGear : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Angler's Fishing Gear");
+            Tooltip.SetDefault(Language.GetTextValue("ItemTooltip.AnglerTackleBag"));
+        }
+
+        public override void SetDefaults() {
+            item.value = Item.sellPrice(0, 7, 0, 0);
+            item.rare = 4;
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual) {
+            // angler tackle bag + angler armor
+            player.accFishingLine = true;
+            player.accTackleBox = true;
+            player.fishingSkill += 25;
+        }
+        
+        public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.AnglerTackleBag);
+            recipe.AddIngredient(ItemID.AnglerHat);
+            recipe.AddIngredient(ItemID.AnglerPants);
+            recipe.AddIngredient(ItemID.AnglerVest);
+            recipe.AddTile(TileID.TinkerersWorkbench);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
