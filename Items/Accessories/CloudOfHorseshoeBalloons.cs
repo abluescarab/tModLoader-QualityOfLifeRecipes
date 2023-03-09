c92e1eb8f716585a3b672717029e7228686b0ae8
Alana Gilston <abluescarab@users.noreply.github.com>
2023-03-09 05:54:47 -0800

    Added Cloud of Horseshoe Balloons and Ninja Frog Gear (icons WIP)

---
+++ Items/Accessories/CloudOfHorseshoeBalloons.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace QualityOfLifeRecipes.Items.Accessories {
+    // TODO: add icon
+    public class CloudOfHorseshoeBalloons : AccessoryItem {
+        protected override AccessoryEffect[] Effects => new AccessoryEffect[] {
+            AccessoryEffect.FartInABalloon,
+            AccessoryEffect.HoneyBalloon,
+            AccessoryEffect.SharkronBalloon,
+            AccessoryEffect.BundleOfBalloons,
+            AccessoryEffect.LuckyHorseshoe
+        };
+
+        protected override string DisplayNameTranslation => 
+            "{$Mods.QualityOfLifeRecipes.Accessories.CloudOfHorseshoeBalloons}";
+
+        protected override string TooltipTranslation =>
+            "{$Mods.QualityOfLifeRecipes.General.JumpSixTimes}" + "\n" +
+                "{$ItemTooltip.LuckyHorseshoe}" + "\n" +
+                "{$ItemTooltip.ShinyRedBalloon}" + "\n" +
+                "{$ItemTooltip.HoneyComb}";
+
+        protected override int SellPrice => Item.sellPrice(0, 12, 0, 0);
+
+        protected override int Rarity => ItemRarityID.Cyan;
+
+        protected override Ingredient[] Ingredients => new Ingredient[] {
+            new(ModContent.ItemType<CloudOfBalloons>()),
+            new(ItemID.LuckyHorseshoe)
+        };
+    }
+}
---
+++ Items/Accessories/NinjaFrogGear.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace QualityOfLifeRecipes.Items.Accessories {
+    // TODO: add icon
+    public class NinjaFrogGear : AccessoryItem {
+        protected override AccessoryEffect[] Effects => new AccessoryEffect[] {
+            AccessoryEffect.FrogFlipper,
+            AccessoryEffect.MasterNinjaGear
+        };
+
+        protected override string DisplayNameTranslation =>
+            "{$Mods.QualityOfLifeRecipes.Accessories.NinjaFrogGear}";
+
+        protected override string TooltipTranslation =>
+            "{$ItemTooltip.FrogFlipper}" + "\n" +
+            "{$ItemTooltip.MasterNinjaGear}";
+
+        protected override int SellPrice => Item.sellPrice(0, 0, 12, 0);
+
+        protected override int Rarity => ItemRarityID.Yellow;
+
+        protected override Ingredient[] Ingredients => new Ingredient[] {
+            new(ItemID.FrogFlipper),
+            new(ItemID.MasterNinjaGear)
+        };
+    }
+}
